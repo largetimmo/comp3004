4aaf19369983c034c7346a5128bce3ed5c6a5e6e
JUNHAO CHEN <kylechen49@gmail.com>
2017-09-15 02:28:52 +0000

    1.CREATE AND FINISH TWO CORE files 2.CREATE AccountDBHelper and implment 2 basic methods

---
+++ app/src/main/java/com/comp3004groupx/smartaccount/Core/Type.java
@@ -0,0 +1,16 @@
+package com.comp3004groupx.smartaccount.Core;
+
+/**
+ * Created by chenjunhao on 2017/9/14.
+ */
+
+public class Type {
+    private String name;
+    public Type(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
---
+++ app/src/main/java/com/comp3004groupx/smartaccount/module/DAO/AccountDBHelper.java
@@ -0,0 +1,38 @@
+package com.comp3004groupx.smartaccount.module.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.comp3004groupx.smartaccount.Core.Account;
+
+/**
+ * Created by chenjunhao on 2017/9/14.
+ */
+
+public class AccountDBHelper extends SQLiteOpenHelper {
+    private final String NAME = "AccountDBHelper";
+    private static final String DB_NAME = "ACCOUNT.db";
+    private static final int VERSION = 1;
+
+    private AccountDBHelper(Context context){
+        super(context,DB_NAME,null,VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(NAME,"On Create");
+        String sqlquery = "CREATE TABLE IF NOT EXIST ACCOUNT(" +
+                "NAME VARCHAR(20)," +
+                "BALANCE REAL," +
+                "TYPE INTEGER," +
+                "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)";
+        db.execSQL(sqlquery);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(NAME,"WE dont support this");
+    }
+
+}
