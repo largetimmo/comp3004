807e86b69d0161ef4f6fa66099fd7a00209380c2
Fahrayheit <raym0nd@outlook.com>
2017-09-17 03:06:46 +0000

    init more user interface, and basic design of trans view

---
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/Setting.java
@@ -0,0 +1,19 @@
+package com.comp3004groupx.smartaccount.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.comp3004groupx.smartaccount.R;
+
+/**
+ * Created by Owner on 2017-09-16.
+ */
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+    }
+}
+
---
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/Transaction.java
@@ -0,0 +1,18 @@
+package com.comp3004groupx.smartaccount.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.comp3004groupx.smartaccount.R;
+
+/**
+ * Created by Owner on 2017-09-16.
+ */
+
+public class Transaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transaction);
+    }
+}
