25e38ef90d02479177960f82903defb2f5bf5e4c
devalex <devalex@f3796b65-e251-4435-aabb-48e2abcfdc00>
2017-09-17 20:29:05 +0000

    Link the texts in bottom panel to corresponding activities

--- app/src/main/java/com/comp3004groupx/smartaccount/view/MainActivity.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/MainActivity.java
@@ -1,9 +1,15 @@
 package com.comp3004groupx.smartaccount.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.comp3004groupx.smartaccount.R;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView transaction = (TextView)findViewById(R.id.textView3);
+        TextView accounts = (TextView)findViewById(R.id.textView4);
+        TextView setting = (TextView)findViewById(R.id.textView);
+        TextView statistics = (TextView)findViewById(R.id.textView2);
+        transaction.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), Transaction.class);
+                startActivity(intent);
+            }
+        });
+        accounts.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), Account.class);
+                startActivity(intent);
+            }
+        });
+        setting.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), Setting.class);
+                startActivity(intent);
+            }
+        });
+        statistics.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), Statistics.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
