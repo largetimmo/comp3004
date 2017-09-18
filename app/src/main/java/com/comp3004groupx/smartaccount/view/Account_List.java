dc7595b8c0ddc2d0210fd3d85e96a37fa3dcce5a
deveric <34050610+GuanhongW@users.noreply.github.com>
2017-09-18 22:15:49 +0000

    Just add a account_list layout.

---
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/Account_List.java
@@ -0,0 +1,17 @@
+package com.comp3004groupx.smartaccount.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.comp3004groupx.smartaccount.R;
+/**
+ * Created by wuguanhong on 2017-09-18.
+ */
+
+public class Account_List {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
