3dc6a529c46253c20de90f66825a5afe9caa8b90
deveric <34050610+GuanhongW@users.noreply.github.com>
2017-09-20 18:25:55 +0000

    Add some layout files (add_account, transaction_list_account, transction_list_date, transction_list_date, view_transaction)

--- app/src/main/java/com/comp3004groupx/smartaccount/view/View_Transaction.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/View_Transaction.java
@@ -1,17 +1,18 @@
 package com.comp3004groupx.smartaccount.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.comp3004groupx.smartaccount.R;
+
 /**
- * Created by wuguanhong on 2017-09-18.
+ * Created by wuguanhong on 2017-09-20.
  */
 
-public class Account_List extends AppCompatActivity{
+public class View_Transaction extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.transaction);
     }
-
 }
