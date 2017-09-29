57f20e8b4c86498abc689387c0668aa237b8fee0
deveric <34050610+GuanhongW@users.noreply.github.com>
2017-09-29 15:29:49 +0000

    Finish the draft design of account.xml

--- app/src/main/java/com/comp3004groupx/smartaccount/view/Account.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/Account.java
@@ -15,6 +15,7 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account);
+
     }
 }
 
