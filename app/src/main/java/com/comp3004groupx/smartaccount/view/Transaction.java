0cc6ebad287761fdddb6549afca1be1adc6014ac
devalex <devalex@f3796b65-e251-4435-aabb-48e2abcfdc00>
2017-10-09 17:17:06 +0000

    Fixed Bugs in Transaction_List_Account

--- app/src/main/java/com/comp3004groupx/smartaccount/view/Transaction.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/Transaction.java
@@ -1,7 +1,10 @@
 package com.comp3004groupx.smartaccount.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.comp3004groupx.smartaccount.Core.*;
