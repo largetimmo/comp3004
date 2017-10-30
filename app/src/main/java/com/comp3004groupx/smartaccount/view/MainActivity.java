79eb2165f562419049d2765d17a5f626cf6cf19f
devray <devray@f3796b65-e251-4435-aabb-48e2abcfdc00>
2017-10-30 08:27:34 +0000

    Account List finished.

--- app/src/main/java/com/comp3004groupx/smartaccount/view/MainActivity.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         });
         accounts.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Intent intent = new Intent(v.getContext(), Add_Account.class);
+                Intent intent = new Intent(v.getContext(), Account_List.class);
                 startActivity(intent);
             }
         });
