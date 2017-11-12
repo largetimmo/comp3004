fa4577e0bd246d5970d1c2aa066adde524b1e126
JUNHAO CHEN <kylechen49@gmail.com>
2017-11-12 21:22:41 +0000

    Add more methods in PAP

--- app/src/main/java/com/comp3004groupx/smartaccount/module/DAO/PAPDAO.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/module/DAO/PAPDAO.java
@@ -1,7 +1,9 @@
 package com.comp3004groupx.smartaccount.module.DAO;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 
 import com.comp3004groupx.smartaccount.Core.Date;
 import com.comp3004groupx.smartaccount.Core.Transaction;
@@ -13,16 +15,18 @@ import java.util.ArrayList;
  */
 
 public class PAPDAO extends AbstractDAO {
-    private static final String BASIC_SELECT_QUERY ="SELECT TRANS.ID AS ID, ACCOUNT.NAME AS NAME1, TRANS.BALANCE, NOTE, PURCHASETYPE.NAME AS NAME2 FROM PAP INNER JOIN ACCOUNT ON PAP.ACCOUNT = ACCOUNT.ID INNER JOIN PURCHASETYPE ON PAP.TYPE = PURCHASETYPE.ID";
+    //CHECKED =0 = Not Check yet
+    //CHECKED = 1 = already checked
+    private static final String BASIC_SELECT_QUERY ="SELECT CHECKED,TRANS.ID AS ID, ACCOUNT.NAME AS NAME1, TRANS.BALANCE, NOTE, PURCHASETYPE.NAME AS NAME2 FROM PAP INNER JOIN ACCOUNT ON PAP.ACCOUNT = ACCOUNT.ID INNER JOIN PURCHASETYPE ON PAP.TYPE = PURCHASETYPE.ID";
     public PAPDAO(Context context){
         super(context);
         dbname = "PAP";
     }
     public boolean addAutoDesc(Transaction transaction){
         boolean flag = false;
-        String sqlquery = "INSERT INTO PAP(AMOUNT,DATE,TYPE,ACCOUNT,NOTE) VALUES (?,?,(SELECT ID FROM PURCHASETYPE WHERE NAME = ?),(SELECT ID FROM ACCOUNT WHERE NAME = ?),?)";
+        String sqlquery = "INSERT INTO PAP(AMOUNT,DATE,TYPE,ACCOUNT,NOTE,CHECKED) VALUES (?,?,(SELECT ID FROM PURCHASETYPE WHERE NAME = ?),(SELECT ID FROM ACCOUNT WHERE NAME = ?),?,?)";
         try{
-            database.execSQL(sqlquery ,new Object[]{transaction.getAmount(),transaction.getDate(),transaction.getType(),transaction.getAccount(),transaction.getNote()});
+            database.execSQL(sqlquery ,new Object[]{transaction.getAmount(),transaction.getDate(),transaction.getType(),transaction.getAccount(),transaction.getNote(),"0"});
             flag = true;
         }catch (Exception e){
             e.printStackTrace();
@@ -35,13 +39,7 @@ public class PAPDAO extends AbstractDAO {
         try{
             Cursor cursor = database.rawQuery(sqlquery,null);
             while (cursor.moveToNext()){
-                int id = cursor.getInt(cursor.getColumnIndex("ID"));
-                double amount = cursor.getDouble(cursor.getColumnIndex("BALANCE"));
-                Date date = new Date(cursor.getString(cursor.getColumnIndex("DATE")));
-                String purchase_type = cursor.getString(cursor.getColumnIndex("NAMEW"));
-                String account = cursor.getString(cursor.getColumnIndex("NAMEQ"));
-                String note = cursor.getString(cursor.getColumnIndex("NOTE"));
-                Transaction transaction = new Transaction(id,date,amount,account,note,purchase_type);
+                Transaction transaction = parseCursor(cursor);
                 transactions.add(transaction);
             }
         }catch (Exception e){
@@ -71,13 +69,13 @@ public class PAPDAO extends AbstractDAO {
         }
         return flag;
     }
-    public ArrayList<Transaction> getPAPAfterDate(String date_str){
+    public ArrayList<Transaction> getPAPAfter(String date_str){
         ArrayList<Transaction> trans = new ArrayList<>();
-        String sqlquery = "SELECT * FROM PAP WHERE DATE >= ?";
+        String sqlquery = BASIC_SELECT_QUERY+" WHERE DATE >= ?";
         try {
             Cursor cursor = database.rawQuery(sqlquery,new String[]{date_str});
             while (cursor.moveToNext()){
-                Transaction t = TransactionDAO.parseTrans(cursor);
+                Transaction t = parseCursor(cursor);
                 trans.add(t);
             }
         } catch (Exception e) {
@@ -85,9 +83,44 @@ public class PAPDAO extends AbstractDAO {
         }
         return trans;
     }
-    public ArrayList<Transaction> getPAPAfterDate(Date date){
-        return  getPAPAfterDate(date.toString());
+    public ArrayList<Transaction> getPAPAfter(Date date){
+        return  getPAPAfter(date.toString());
     }
+    public ArrayList<Transaction> getUncheckedPAP(){
+        ArrayList<Transaction> trans = new ArrayList<>();
+        String sqlquery = BASIC_SELECT_QUERY + " WHERE CHECKED = 0 ORDER BY DATE DESC";
+        try {
+            Cursor cursor = database.rawQuery(sqlquery,null);
+            while (cursor.moveToNext()){
+                Transaction t = parseCursor(cursor);
+                trans.add(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return trans;
+    }
+    private Transaction parseCursor (Cursor cursor){
+        int id = cursor.getInt(cursor.getColumnIndex("ID"));
+        double amount = cursor.getDouble(cursor.getColumnIndex("BALANCE"));
+        Date date = new Date(cursor.getString(cursor.getColumnIndex("DATE")));
+        String purchase_type = cursor.getString(cursor.getColumnIndex("NAMEW"));
+        String account = cursor.getString(cursor.getColumnIndex("NAMEQ"));
+        String note = cursor.getString(cursor.getColumnIndex("NOTE"));
+        return new Transaction(id,date,amount,account,note,purchase_type);
+    }
+    public boolean checkPAP(int id){
+        boolean flag = false;
+        String sqlquery = "UPDATE PAP SET CHECKED = 1 WHERE ID = ?";
+        try {
+            database.execSQL(sqlquery,new Object[]{Integer.toString(id)});
+            flag = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
 
 
 }
