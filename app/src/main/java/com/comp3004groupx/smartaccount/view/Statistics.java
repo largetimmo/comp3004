bb487f9e55883fe01505ca1a33b0fb510290586f
devray <devray@f3796b65-e251-4435-aabb-48e2abcfdc00>
2017-11-16 00:39:13 +0000

    statistic setting area onchange event finished, charts generator in progress.

--- app/src/main/java/com/comp3004groupx/smartaccount/view/Statistics.java
+++ app/src/main/java/com/comp3004groupx/smartaccount/view/Statistics.java
@@ -1,24 +1,40 @@
 package com.comp3004groupx.smartaccount.view;
 
 import android.animation.ValueAnimator;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
-import android.widget.CompoundButton;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.comp3004groupx.smartaccount.R;
+import com.comp3004groupx.smartaccount.module.DAO.AccountDAO;
+import com.comp3004groupx.smartaccount.module.DAO.AccountTypeDAO;
 import com.comp3004groupx.smartaccount.module.DAO.TransactionDAO;
+
+
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import android.app.DatePickerDialog;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by devray on 2017-09-16.
@@ -27,14 +43,47 @@ import java.util.ArrayList;
 
 public class Statistics extends AppCompatActivity {
 
-
-    ToggleButton setButton;
+    //setting Area element
     LinearLayout settingArea;
+    ToggleButton setButton;
+
+    RadioGroup chartInfoGroup;
+    RadioButton selectTrans;
+    RadioButton selectBalance;
+
+    LinearLayout transArea;
+    CheckBox checkExp;
+    CheckBox checkInc;
+
+    LinearLayout balArea;
+    CheckBox checkProp;
+    CheckBox checkLiab;
+
+    CheckBox checkTimeLine;
+    static Date startDate;
+    static Date endDate;
+    static Calendar calendar;
+    static String textOnClick;
+    static TextView startDateText;
+    static TextView endDateText;
+
+    static final String myFormat = "yyyy-MM-dd";
+    static final SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.CANADA);
+
+    RadioGroup chartTypeGroup;
+    RadioButton selectBar;
+    RadioButton selectPie;
+    RadioButton selectLine;
+
+
+    //table Area element
     LinearLayout tableArea;
 
     PieChart piechart;
 
     TransactionDAO transDataBase;
+    AccountDAO accountDataBase;
+    AccountTypeDAO accountTypeDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +91,183 @@ public class Statistics extends AppCompatActivity {
         setContentView(R.layout.statistics);
         this.setTitle("Statistics");
 
+        settingAreaEvent();
+        setup();
+    }
+
+
+    // setup default condition
+    //----------------------------------------------------------------------------------------------
+    private void setup() {
+        //setting area expend as default
+        setButton = (ToggleButton) findViewById(R.id.setupTableButton);
+        setButton.setChecked(true);
+
+        //select transaction as default
+        selectTrans = (RadioButton) findViewById(R.id.selectTransaction);
+        selectTrans.setChecked(true);
+
+        //select bar chart as default
+        selectBar = (RadioButton) findViewById(R.id.selectBarChart);
+        selectBar.setChecked(true);
+
+        //default date for timeline option
+        calendar = Calendar.getInstance();
+        endDate = new Date(calendar.getTime().getTime());
+        calendar.set(Calendar.MONTH, Calendar.getInstance().get(Calendar.MONTH) - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        startDate = new Date(calendar.getTime().getTime());
+        //default date text
+        startDateText.setText(sdf.format(startDate.getTime()));
+        endDateText.setText(sdf.format(endDate.getTime()));
+
+    }
+
+    // setting area event listener
+    //----------------------------------------------------------------------------------------------
+    private void settingAreaEvent() {
+        settingAreaControl();
+        ChartOptionEvent();
+    }
+
+    //expand and collapse setting area and update table after collapse
+    private void settingAreaControl() {
         setButton = (ToggleButton) findViewById(R.id.setupTableButton);
         settingArea = (LinearLayout) findViewById(R.id.tableSettingArea);
+
+        selectTrans = (RadioButton) findViewById(R.id.selectTransaction);
+        selectBalance = (RadioButton) findViewById(R.id.selectBalance);
+        checkExp = (CheckBox) findViewById(R.id.checkExp);
+        checkInc = (CheckBox) findViewById(R.id.checkInc);
+        checkProp = (CheckBox) findViewById(R.id.checkProperty);
+        checkLiab = (CheckBox) findViewById(R.id.checkLiability);
+        selectBar = (RadioButton) findViewById(R.id.selectBarChart);
+        selectPie = (RadioButton) findViewById(R.id.selectPieChart);
+        selectLine = (RadioButton) findViewById(R.id.selectLineChart);
+        checkTimeLine = (CheckBox) findViewById(R.id.checkTimeLine);
+
+        String chaType;
+        if (selectBar.isChecked()) {
+            chaType = "Bar";
+        } else if (selectLine.isChecked()) {
+            chaType = "Line";
+        } else if (selectPie.isChecked()) {
+            chaType = "Pie";
+        } else {
+            chaType = "";
+        }
+
+        setButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                //expand
+                expandOrCollapse(settingArea, "height", 300, dpToPx(600));
+            } else {
+                //update chart
+                updateChart(selectTrans.isChecked(), checkExp.isChecked(), checkInc.isChecked(),
+                        checkProp.isChecked(), checkLiab.isChecked(), chaType, checkTimeLine.isChecked());
+                //collapse
+                expandOrCollapse(settingArea, "height", 300, dpToPx(40));
+
+            }
+        });
+    }
+
+    //chart option onchange event
+    private void ChartOptionEvent() {
+        chartInfoGroup = (RadioGroup) findViewById(R.id.chartInfoGroup);
+        transArea = (LinearLayout) findViewById(R.id.checkTransLayout);
+        balArea = (LinearLayout) findViewById(R.id.checkBalanceLayout);
+
+        checkTimeLine = (CheckBox) findViewById(R.id.checkTimeLine);
+        startDateText = (TextView) findViewById(R.id.startDateText);
+        endDateText = (TextView) findViewById(R.id.endDateText);
+
+        chartTypeGroup = (RadioGroup) findViewById(R.id.chartTypeGroup);
+        selectBar = (RadioButton) findViewById(R.id.selectBarChart);
+        selectPie = (RadioButton) findViewById(R.id.selectPieChart);
+        selectLine = (RadioButton) findViewById(R.id.selectLineChart);
+
+        //area control of two chart
+        chartInfoGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            // expend and collapse area
+            if (checkedId == R.id.selectTransaction) {
+                expandOrCollapse(balArea, "width", 300, dpToPx(0));
+                expandOrCollapse(transArea, "width", 300, dpToPx(250));
+            } else if (checkedId == R.id.selectBalance) {
+                expandOrCollapse(transArea, "width", 300, dpToPx(0));
+                expandOrCollapse(balArea, "width", 300, dpToPx(250));
+            }
+
+        });
+
+        //pie chart disabled for timeline
+        checkTimeLine.setOnCheckedChangeListener((ButtonView, isChecked) -> {
+            if (isChecked) {
+                selectLine.setChecked(true);
+                selectPie.setClickable(false);
+            } else {
+                selectPie.setClickable(true);
+            }
+
+        });
+
+        //click on date text
+        startDateText.setOnClickListener((v) -> {
+            textOnClick = "start";
+            DialogFragment datepickerDialog = new DatePickerFragment();
+            datepickerDialog.show(getFragmentManager(), "datePicker");
+        });
+        endDateText.setOnClickListener((v) -> {
+            textOnClick = "end";
+            DialogFragment datepickerDialog = new DatePickerFragment();
+            datepickerDialog.show(getFragmentManager(), "datePicker");
+        });
+
+    }
+
+
+    // chart area event listener
+    //----------------------------------------------------------------------------------------------
+
+    //update chart
+    private void updateChart(boolean isTrans,
+                             boolean checkExp, boolean checkInc, boolean checkProp, boolean checkLiab,
+                             String chartType, boolean checkTime) {
+        tableArea = (LinearLayout) findViewById(R.id.tableArea);
+
+
+        transDataBase = new TransactionDAO(getApplicationContext());
+        accountDataBase = new AccountDAO(getApplicationContext());
+        accountTypeDataBase = new AccountTypeDAO((getApplicationContext()));
+
+
+        if (chartType.equals("Bar") && checkExp && checkInc) {
+
+        } else if (chartType.equals("Bar") && checkExp && !checkInc) {
+
+        } else if (chartType.equals("Bar") && !checkExp && checkInc) {
+
+        } else if (chartType.equals("Bar") && !checkExp && !checkInc) {
+
+        } else {
+
+        }
+
+
+        if (tableArea.getChildCount() > 0) {
+            tableArea.removeAllViews();
+        }
+        //TODO: add chart
+
+    }
+
+    //make a new chart
+    private void makeChart() {
+
+
         tableArea = (LinearLayout) findViewById(R.id.tableArea);
         piechart = (PieChart) findViewById(R.id.piechart);
 
-        transDataBase = new TransactionDAO(getApplicationContext());
-
-        setup(settingArea, setButton);
-
-        maketable(piechart, transDataBase);
-    }
-
-    private void maketable(PieChart pieChart, TransactionDAO transDataBase) {
 
         ArrayList<PieEntry> pieChartEntries = new ArrayList<>();
 
@@ -78,81 +291,15 @@ public class Statistics extends AppCompatActivity {
 
 
         PieData data = new PieData(dataSet);
-        pieChart.setData(data);
-        pieChart.invalidate(); // refresh
+        piechart.setData(data);
+        piechart.invalidate(); // refresh
 
 
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void setup(final LinearLayout settingArea, ToggleButton setButton) {
-
-        setButton.setChecked(false);
-
-        settingAreaControl(settingArea, setButton);
-
-    }
-
-    private void settingAreaControl(final LinearLayout settingArea, ToggleButton setButton) {
-
-        setButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    expand(settingArea, 300, dpToPx(180));
-                } else {
-                    collapse(settingArea, 300, dpToPx(40));
-                }
-            }
-        });
-    }
-
-    public static void expand(final View v, int duration, int targetHeight) {
-        int prevHeight = v.getHeight();
-        v.setVisibility(View.VISIBLE);
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(prevHeight, targetHeight);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                v.getLayoutParams().height = (int) animation.getAnimatedValue();
-                v.requestLayout();
-            }
-        });
-        valueAnimator.setInterpolator(new DecelerateInterpolator());
-        valueAnimator.setDuration(duration);
-        valueAnimator.start();
-    }
-
-    public static void collapse(final View v, int duration, int targetHeight) {
-        int prevHeight = v.getHeight();
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(prevHeight, targetHeight);
-        valueAnimator.setInterpolator(new DecelerateInterpolator());
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                v.getLayoutParams().height = (int) animation.getAnimatedValue();
-                v.requestLayout();
-            }
-        });
-        valueAnimator.setInterpolator(new DecelerateInterpolator());
-        valueAnimator.setDuration(duration);
-        valueAnimator.start();
-    }
-
+    // tools for help
+    //----------------------------------------------------------------------------------------------
     public static int dpToPx(int dp) {
         return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
@@ -160,5 +307,85 @@ public class Statistics extends AppCompatActivity {
     public static int pxToDp(int px) {
         return (int) (px / Resources.getSystem().getDisplayMetrics().density);
     }
+
+    // type must be "height" or "width"
+    public static void expandOrCollapse(final View v, final String type, int duration, int targetLength) {
+        int prevLength;
+
+        if (type.equals("height")) {
+            prevLength = v.getHeight();
+
+        } else if (type.equals("width")) {
+            prevLength = v.getWidth();
+        } else {
+            prevLength = 0;
+        }
+
+        v.setVisibility(View.VISIBLE);
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(prevLength, targetLength);
+        valueAnimator.addUpdateListener((animation) -> {
+            if (type.equals("height")) {
+                v.getLayoutParams().height = (int) animation.getAnimatedValue();
+            } else if (type.equals("width")) {
+                v.getLayoutParams().width = (int) animation.getAnimatedValue();
+            }
+            v.requestLayout();
+
+        });
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.setDuration(duration);
+        valueAnimator.start();
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            calendar = Calendar.getInstance();
+            int year;
+            int month;
+            int day;
+            DatePickerDialog picker;
+
+            if (textOnClick.equals("start")) {
+                calendar.setTime(startDate);
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                picker = new DatePickerDialog(getActivity(), this, year, month, day);
+                picker.getDatePicker().setMaxDate(endDate.getTime());
+
+            } else if (textOnClick.equals("end")) {
+                calendar.setTime(endDate);
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                picker = new DatePickerDialog(getActivity(), this, year, month, day);
+                picker.getDatePicker().setMinDate(startDate.getTime());
+                picker.getDatePicker().setMaxDate(Calendar.getInstance().getTime().getTime());
+            } else {
+                picker = new DatePickerDialog(getActivity(), this, 0, 0, 0);
+            }
+            return picker;
+        }
+
+        //textOnClick must be "start" or "end"
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            calendar = Calendar.getInstance();
+
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, day);
+
+            if (textOnClick.equals("start")) {
+                startDate.setTime(calendar.getTime().getTime());
+                startDateText.setText(sdf.format(calendar.getTime()));
+            } else if (textOnClick.equals("end")) {
+                endDate.setTime(calendar.getTime().getTime());
+                endDateText.setText(sdf.format(calendar.getTime()));
+            }
+        }
+    }
+
 }
 
